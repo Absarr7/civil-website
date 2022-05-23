4fd4a642f4c9573c64d629269d65b22f15894164
Absar Ahmad <absar4060@gmail.com>
2022-05-23 21:39:01 +0530

    about us section created

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,33 @@ hamburger.addEventListener('click', function() {
 
 
 })
+
+
+// COUNTER NUMBER ANIMATION
+
+const numbers = document.querySelectorAll('.numbers');
+const speed = 200;
+
+numbers.forEach(counter => {
+    const updateNumbers = () => {
+        const target = counter.getAttribute('data-target');
+
+        console.log(target);
+    }
+
+    updateNumbers();
+});
+
+// About section Read More
+
+const readMoreBtn = document.querySelector('.read-more-button');
+const aboutText = document.querySelector('.about-text-p');
+
+readMoreBtn.addEventListener('click', (e) => {
+    aboutText.classList.toggle('show-more');
+    if (readMoreBtn.innerText === "Read More...") {
+        readMoreBtn.innerText = "Read Less...";
+    } else {
+        readMoreBtn.innerText = "Read More...";
+    }
+});
